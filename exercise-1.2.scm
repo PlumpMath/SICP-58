1f2e9783039c26944175f93c88395b98607fec40
J. Cohen <hostelmarketing@gmail.com>
2014-06-10 15:08:17 -0700

    Edit 1.2

--- exercise-1.2.scm
+++ exercise-1.2.scm
@@ -1,5 +1,6 @@
 ; Exercise 1.2 from SICP
-; I didn't check the correctness, but it evaluates to -37/150
+; I didn't check the correctness, but it evaluates to -37/150 in Scheme
+; With a calculator: 74/5 * -60 = -888
 
 (/
   (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) ; 74/5
