f1be37a41e4f8d0b72e70ae6d44c0b480f23e014
J. Cohen <hostelmarketing@gmail.com>
2014-06-10 19:35:54 -0700

    Add exercises 1.4 and 1.5

---
+++ exercises/exercise-1.4.scm
@@ -0,0 +1,3 @@
+; Exercise 1.4 from SICP
+
+; Returns the operator dynamically. Either the answer goes without saying or I'm missing something.
---
+++ exercises/exercise-1.5.scm
@@ -0,0 +1,12 @@
+; Exercise 1.5 from SICP
+
+; The code given is:
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+    0
+    y))
+
+; If it's applicative-order evaluation, the next line will run the infinitely recursive function first. If it's normal-order evaluation, it will return 0 without running (p). Scheme uses the former by default.
+(test 0 (p))
