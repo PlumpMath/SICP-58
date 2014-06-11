307a75eda97e869b5d00146ace8aa9b6d3b2b252
J. Cohen <hostelmarketing@gmail.com>
2014-06-10 17:19:30 -0700

    Refactor exercise 1.3

--- exercise-1.3.scm
+++ exercise-1.3.scm
@@ -2,6 +2,9 @@
 
 ; Accept three numbers and return the larger two
 (define (sum-of-squares-largest-two-of-three x y z)
-  (cond ((or (< x y) (< x z)) (+ (square y) (square z)))
-        ((or (< y x) (< y z)) (+ (square x) (square z)))
-        (else (+ (square x) (square y)))))
+  (cond ((or (< x y) (< x z)) (sum-of-squares y z))
+        ((or (< y x) (< y z)) (sum-of-squares x z))
+        (else (sum-of-squares x y))))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
