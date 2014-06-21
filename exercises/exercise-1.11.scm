4357f353df08512ff06d5d625a516ccf60414d4b
J. Cohen <hostelmarketing@gmail.com>
2014-06-21 10:29:10 -0700

    Add TODO

--- exercises/exercise-1.11.scm
+++ exercises/exercise-1.11.scm
@@ -1,12 +1,17 @@
 ; Exercise 1.11 from SICP
 
 ; Formula:
-; f(n = n if n < 3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3)
+; f(n) = n if n < 3
+; and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3
 
 ; Write a recursive version
+; TODO: this is probably wrong, because it seems to return 3 for everything
 (define (f n)
   (cond (< n 3) (n)
-        (>= n 3) (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))
+        (>= n 3) (+
+                   (f (- n 1))
+                   (* 2 (f (- n 2)))
+                   (* 3 (f (- n 3))))))
 
 
 ; Write an iterative version
