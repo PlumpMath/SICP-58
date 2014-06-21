e8eef397a2170656a9899fb332e9da9a5a4827ba
J. Cohen <hostelmarketing@gmail.com>
2014-06-21 10:25:04 -0700

    Recursive version of 1.11, not tested

--- exercises/exercise-1.11.scm
+++ exercises/exercise-1.11.scm
@@ -1,2 +1,12 @@
-; Exercise 1.12 from SICP
+; Exercise 1.11 from SICP
 
+; Formula:
+; f(n = n if n < 3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3)
+
+; Write a recursive version
+(define (f n)
+  (cond (< n 3) (n)
+        (>= n 3) (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))
+
+
+; Write an iterative version
