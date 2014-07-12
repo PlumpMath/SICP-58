a5543157772f8e4a091b1931409a3d3e678210b8
J. Cohen <hostelmarketing@gmail.com>
2014-07-12 09:49:30 -0700

    Higher order procedures

--- exercises/sandbox/higherorder.scm
+++ exercises/sandbox/higherorder.scm
@@ -1,25 +1,39 @@
 ;;;; From the higher-order procedures section in 1.3
 
-(define (sum-integers a b)
+;; (define (sum-integers a b)
+;;   (if (> a b)
+;;       0
+;;       (+ a (sum-integers (+ a 1) b)))) 
+
+;; (define (sum-cubes a b)
+;;   (if (> a b)
+;;       0
+;;       (+ (cube a)
+;; 	 (sum-cubes (+ a 1) b))))
+
+;; (define (pi-sum a b)
+;;   (if (> a b)
+;;       0
+;;       (+ (/ 1.0 (* a (+ a 2)))
+;; 	 (pi-sum (+ a 4) b))))
+
+;; ; The basic pattern
+;; (define (<name> a b)
+;;   (if (> a b)
+;;       0
+;;       (+ (<term> a)
+;; 	 (<name> (<next> a) b))))
+
+; Becomes:
+(define (sum term a next b)
   (if (> a b)
       0
-      (+ a (sum-integers (+ a 1) b)))) 
+      (+ (term a)
+	 (sum term (next a) next b)))) 
 
+(define (inc n) (+ n 1)) 
 (define (sum-cubes a b)
-  (if (> a b)
-      0
-      (+ (cube a)
-	 (sum-cubes (+ a 1) b))))
+  (sum cube a inc b)) 
 
-(define (pi-sum a b)
-  (if (> a b)
-      0
-      (+ (/ 1.0 (* a (+ a 2)))
-	 (pi-sum (+ a 4) b))))
-
-; The basic pattern
-(define (<name> a b)
-  (if (> a b)
-      0
-      (+ (<term> a)
-	 (<name> (<next> a) b))))
+(define (cube a)
+  (* a a a)) 
