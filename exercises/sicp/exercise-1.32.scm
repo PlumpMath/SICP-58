c430a514357d8b9b4b0ffaddb0c93592f81e6051
J. Cohen <hostelmarketing@gmail.com>
2014-07-19 10:10:27 -0700

    More edits

--- exercises/sicp/exercise-1.32.scm
+++ exercises/sicp/exercise-1.32.scm
@@ -22,3 +22,12 @@
 
 (define (product term a next b)
   (accumulate * 1 term a next b))
+
+;;; Iterative version from wiki:
+(define (accumulate combiner null-value term a next b)
+  (define (iter a res)
+    (if (> a b)
+	res
+	(iter (next a) (combiner res (term a)))))
+  (iter a null-value))
+	
