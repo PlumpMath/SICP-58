da3976d513ef84ced93ebf4738522fc656bc9cd2
J. Cohen <hostelmarketing@gmail.com>
2014-07-19 09:28:11 -0700

    Merge files

--- exercises/sicp/exercise-1.31.scm
+++ exercises/sicp/exercise-1.31.scm
@@ -51,3 +51,19 @@
 (define (identity x) x) 
 (define (sum-integers a b)
   (sum identity a inc b))
+
+;;;; Another attempt
+;;;
+
+(define (product f a next b)
+  (if (> a b)
+    1
+    (* (f a)
+       (product f (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (cube n) (* n n n))
+(define (identity x) x) ; Allows returning the argument without running it (no op)
+
+(define (factorial n)
+  (product identity 0 inc n))
